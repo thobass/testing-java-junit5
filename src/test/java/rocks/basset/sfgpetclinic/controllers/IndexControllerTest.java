83b1d862817c2e6c732e622584c59b59a6b8dfe5
BASSET, Thomas <thomas.basset@cgi.com>
2022-11-17 09:10:11 +0100

    JUnit assertions

---
+++ src/test/java/rocks/basset/sfgpetclinic/controllers/IndexControllerTest.java
@@ -0,0 +1,31 @@
+package rocks.basset.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexControllerTest {
+
+    IndexController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new IndexController();
+    }
+
+    @Test
+    void index() {
+        assertEquals("index", controller.index());
+        assertEquals("index", controller.index(), "Wrong view returned.");
+        assertEquals("index", controller.index(), () -> "Another Expensive Message " +
+                "Make me only if you have to");
+    }
+
+    @Test
+    void oupsHandler() {
+        assertTrue("notimplemented".equals(controller.oupsHandler()), () -> "This is some expensive" +
+                "Message to build" +
+                "for my test");
+    }
+}
