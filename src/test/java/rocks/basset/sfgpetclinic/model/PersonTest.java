7c8924c390c4baa6bec1c9708ace9a5666411bff
BASSET, Thomas <thomas.basset@cgi.com>
2022-11-18 09:15:02 +0100

    Grouped assertions

---
+++ src/test/java/rocks/basset/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,30 @@
+package rocks.basset.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @Test
+    void groupAssertions(){
+        //Given
+        Person p = new Person(1l, "Joe","Buck");
+        //Then
+        assertAll("Test Props Set",
+                () -> assertEquals("Joe", p.getFirstName()),
+                () -> assertEquals("Buck", p.getLastName())
+                );
+    }
+
+    @Test
+    void groupAssertionsMsgs(){
+        //Given
+        Person p = new Person(1l, "Joe","Buck");
+        //Then
+        assertAll("Test Props Set",
+                () -> assertEquals("Joe", p.getFirstName(), "First Name Failed"),
+                () -> assertEquals("Buck", p.getLastName(), "Last Name Failed")
+        );
+    }
+}
