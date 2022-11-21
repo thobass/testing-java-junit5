c2f630e0183e684d00899374a469d7eb96310015
BASSET, Thomas <thomas.basset@cgi.com>
2022-11-21 09:03:14 +0100

    JUnit test display name

--- src/test/java/rocks/basset/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/rocks/basset/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,6 +1,7 @@
 package rocks.basset.sfgpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,6 +16,7 @@ class IndexControllerTest {
     }
 
     @Test
+    @DisplayName("Test proper View name is returned for index page")
     void index() {
         assertEquals("index", controller.index());
         assertEquals("index", controller.index(), "Wrong view returned.");
@@ -23,6 +25,7 @@ class IndexControllerTest {
     }
 
     @Test
+    @DisplayName("Test exception")
     void oupsHandler() {
         assertTrue("notimplemented".equals(controller.oupsHandler()), () -> "This is some expensive" +
                 "Message to build" +
