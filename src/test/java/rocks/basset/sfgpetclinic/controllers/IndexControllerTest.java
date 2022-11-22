dad8956c5fe000705f5bd5b98ba5c511830b6f57
BASSET, Thomas <thomas.basset@cgi.com>
2022-11-22 08:19:16 +0100

    Testing timeouts

--- src/test/java/rocks/basset/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/rocks/basset/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,8 +1,11 @@
 package rocks.basset.sfgpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,6 +32,24 @@ class IndexControllerTest {
     void oupsHandler() {
         assertThrows(ValueNotFoundException.class, () -> {
             controller.oupsHandler();
+        });
+    }
+
+    @Test
+    @Disabled("Demo for timeout")
+    void testTimeout(){
+        assertTimeout(Duration.ofMillis(100), () -> {
+            Thread.sleep(5000);
+            System.out.println("I got there");
+        });
+    }
+
+    @Test
+    @Disabled("Demo for timeout")
+    void testTimeoutPreempt(){
+        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+            Thread.sleep(5000);
+            System.out.println("I got there preemptivly");
         });
     }
 }
