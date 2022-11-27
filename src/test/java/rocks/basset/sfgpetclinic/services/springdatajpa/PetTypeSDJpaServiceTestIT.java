4eebcdf31b41f9153a4ce371ce9184d192a635da
BASSET, Thomas <thomas.basset@cgi.com>
2022-11-27 17:48:13 +0100

    Unit Tests vs Integration Tests

---
+++ src/test/java/rocks/basset/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceTestIT.java
@@ -0,0 +1,33 @@
+package rocks.basset.sfgpetclinic.services.springdatajpa;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PetTypeSDJpaServiceTestIT {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
