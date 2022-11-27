cb261682b603b91c3e8f029fe818ba68122bec34
BASSET, Thomas <thomas.basset@cgi.com>
2022-11-27 11:49:42 +0100

    junit parameterized tests - csv source

--- src/test/java/rocks/basset/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/rocks/basset/sfgpetclinic/model/OwnerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import rocks.basset.sfgpetclinic.ControllerTests;
@@ -48,5 +49,16 @@ class OwnerTest implements ModelTests {
     @EnumSource(OwnerType.class)
     void testEnumSource(OwnerType ownerType){
         System.out.println(ownerType);
+    }
+
+    @DisplayName("Csv Source Test")
+    @ParameterizedTest(name= ParameterizedTest.DISPLAY_NAME_PLACEHOLDER + " - " + ParameterizedTest.DEFAULT_DISPLAY_NAME)
+    @CsvSource({
+            "FL,1,1",
+            "OH,2,2",
+            "MI,1,1"
+    })
+    void testCsvSource(String stateName, int val1, int val2){
+        System.out.println(stateName + " = " + val1 + " : " + val2);
     }
 }
